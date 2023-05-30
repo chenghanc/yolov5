c7bd822e603df462750c6beb9c351f895a59e21a
Cesare <chenghan.chung@rwth-aachen.de>
2023-05-30 16:13:56 +0800

    add PP-LC

--- models/common.py
+++ models/common.py
@@ -42,6 +42,31 @@ def autopad(k, p=None, d=1):  # kernel, padding, dilation
     return p
 
 #-------------------------------------------------------------------------
+# PP-LCNet
+
+class DepthSepConv(nn.Module):
+    def __init__(self, inp, oup, dw_size, stride, use_se):
+        super(DepthSepConv, self).__init__()
+        self.stride = stride
+        self.inp = inp
+        self.oup = oup
+        self.dw_size = dw_size
+        self.dw_sp = nn.Sequential(
+            nn.Conv2d(self.inp, self.inp, kernel_size=self.dw_size, stride=self.stride, padding=(dw_size - 1) // 2, groups=self.inp, bias=False),
+            nn.BatchNorm2d(self.inp),
+            nn.Hardswish(),
+
+            SeBlock(self.inp, reduction=16) if use_se else nn.Sequential(),
+
+            nn.Conv2d(self.inp, self.oup, kernel_size=1, stride=1, padding=0, bias=False),
+            nn.BatchNorm2d(self.oup),
+            nn.Hardswish())
+
+    def forward(self, x):
+        y = self.dw_sp(x)
+        return y
+
+#-------------------------------------------------------------------------
 # EfficientNetLite
 class drop_connect:
     def __init__(self, drop_connect_rate):
